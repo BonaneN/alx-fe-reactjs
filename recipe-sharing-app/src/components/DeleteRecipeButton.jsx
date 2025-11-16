cc881151e868b266df68933f5bb7872e4f9f029b
BonaneN <rbonane336@gmail.com>
2025-11-16 20:47:14 +0200

    bug fix

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import useRecipeStore from "../recipeStore";
 
 const DeleteRecipeButton = ({ id }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();   // <-- REQUIRED
 
   const handleDelete = () => {
     deleteRecipe(id);
+    navigate("/");                  // Redirect after deletion
   };
 
   return (
