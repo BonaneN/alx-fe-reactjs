3b646ac342125a9dc8d53ff95b4a616bf4fd2011
BonaneN <rbonane336@gmail.com>
2025-12-14 09:40:29 +0200

    Add controlled form and Formik registration form

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,9 @@
+import FormikForm from "./components/formikForm";
+import RegistrationForm from "./components/RegistrationForm";
+
+function App() {
+  return <FormikForm />;
+  return <RegistrationForm />;
+}
+
+export default App;
